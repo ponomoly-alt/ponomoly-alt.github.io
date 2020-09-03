277cd74298c08848d249ce9f08f568b077902815
ponomoly-alt <ponomoly1121@gmail.com>
2020-09-03 22:36:27 +0900

    Updates

---
+++ precache-manifest.29a963df98cdb9464c1beef7a01e16b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8ee7b9e787b87bb6cf9ebd7d00a648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff615a9c735c0496d5f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92155ba12620cb46c216",
+    "url": "/static/js/2.75d23d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75d23d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff615a9c735c0496d5f",
+    "url": "/static/js/main.8ccc6fff.chunk.js"
+  },
+  {
+    "revision": "68ee1f524f49a711ddc5",
+    "url": "/static/js/runtime-main.936283e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
